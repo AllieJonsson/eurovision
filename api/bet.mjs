137e0232aedc0e880790d118fb81f64e35d5bd1a
Alice Jonsson <10475857+AllieJonsson@users.noreply.github.com>
2025-05-06 14:55:43 +0200

    get :O

--- api/bet.mjs
+++ api/bet.mjs
@@ -10,10 +10,15 @@ const redis = new Redis({
   token: process.env.KV_REST_API_TOKEN || '',
 });
 
-export default function handler(request, response, x, y, z) {
+export default async function handler(request, response, x, y, z) {
   console.log(request);
-  // if (request.method === 'GET') {
-  //     redis.get()
-  // }
-  return new Response(JSON.stringify({ text: 'Hello world!' }), { status: 200 });
+  const url = new URL(request.url);
+  if (request.method === 'GET') {
+    const id = url.searchParams.get('id');
+    if (!id) return new Response(JSON.stringify({ error: 'Missing param: id' }), { status: 400 });
+    const cached = await redis.get(`bet.${id}`);
+    return new Response(JSON.stringify({ bet: cached ? JSON.parse(cached) : [] }), { status: 200 });
+  } else if (request.method === 'POST') {
+    console.log(await request.body.json());
+  }
 }
