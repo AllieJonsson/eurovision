8371d6a450b7d96d9da2a42e4866a575e1654e90
Alice Jonsson <10475857+AllieJonsson@users.noreply.github.com>
2025-05-16 10:10:32 +0200

    finare dnd

--- api/bet.mjs
+++ api/bet.mjs
@@ -22,6 +22,9 @@ export default async function handler(request) {
   } else if (request.method === 'POST') {
     const data = await request.json();
     console.log(data);
+    if (new Date() > new Date('2025-05-17T21:00:00Z')) {
+      return new Response(JSON.stringify({ error: 'Betting is closed' }), { status: 403 });
+    }
     if (!data.id) return new Response(JSON.stringify({ error: 'Missing param: id' }), { status: 400 });
     if (!data.bet || !Array.isArray(data.bet))
       return new Response(JSON.stringify({ error: 'Missing param: bet' }), { status: 400 });
