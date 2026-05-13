18d0e42b3151b0ba43a4a7d166116cecdc1c8457
Alice Jonsson <10475857+AllieJonsson@users.noreply.github.com>
2026-05-13 13:08:03 +0200

    flag

--- result.js
+++ result.js
@@ -32,7 +32,9 @@ const createResultEntry = (entry, result, users) => {
     answerClone.classList.remove('hidden');
     answerClone.firstElementChild.textContent = user.name;
     const country = entries.find((e) => e.id === bet.id);
-    answerClone.firstElementChild.nextElementSibling.classList.add(`bg-[url(https://flagcdn.com/h60/un.png)]`);
+    answerClone.firstElementChild.nextElementSibling.classList.add(
+      `bg-[url(https://flagcdn.com/h60/${country?.code ?? 'un'}.png)]`,
+    );
     answerClone.firstElementChild.nextElementSibling.nextElementSibling.textContent = country?.country ?? '???';
     if (areResultsIn) {
       const element =
